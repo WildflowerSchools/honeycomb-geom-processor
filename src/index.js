8f15b731c4aa3b5729c47b285f6bbef3311ed7b3
Ben Talberg <ben.talberg@gmail.com>
2020-02-14 17:23:36 -0800

    resolve cert issue on prod

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 require("dotenv").config()
 
 const express = require("express")
-const https = require("https")
 
 const auth = require("./auth")
 const geoms = require("./geoms")
@@ -12,22 +11,23 @@ const applyGeoms = geoms.apply
 
 const port = process.env.SERVICE_PORT ? process.env.SERVICE_PORT : 8010
 
-let app
+let app, server
 ;(async () => {
+
   if (process.env.ENVIRONMENT === "production") {
+    const http = require("http")
     app = express()
+    server = http.createServer(app)
   } else {
+    const https = require("https")
     app = require("https-localhost")()
+    server = https.createServer(await app.getCerts(), app)
   }
 
   console.log("setting up")
 
   applyAuth(app)
   applyErrors(app)
-
-  const certs = await app.getCerts()
-  const server = https.createServer(certs, app)
-
   applyGeoms(server)
 
   server.listen(port, function() {
